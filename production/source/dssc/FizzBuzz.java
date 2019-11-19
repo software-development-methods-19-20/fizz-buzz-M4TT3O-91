7f91e7b7811f986635a08883716bc0d60bbed873
Matteo <teo_exc@hotmail.it>
2019-11-19 17:15:19 +0100

    - Implement all Test

--- production/source/dssc/FizzBuzz.java
+++ production/source/dssc/FizzBuzz.java
@@ -1,10 +1,22 @@
 package dssc;
 
 public class FizzBuzz {
-    public static String test(String input) {
+
+    public void FizzBuzzPrint(){
+        for (int i = 0; i < 105 ; i++) {
+            System.out.println(test(i));
+        }
+    }
+
+    public static String test(Integer input) {
         String test = "";
-        test += (Integer.parseInt(input) % 3) == 0 ? "fizz" : "";
-        test += (Integer.parseInt(input) % 5) == 0 ? "buzz" : "";
+        if (input == 0)
+            return "";
+        else {
+            test += (input % 3) == 0 ? "Fizz" : "";
+            test += (input % 5) == 0 ? "Buzz" : "";
+            test += (input % 7) == 0 ? "Bang" : "";
+        }
         return test;
     }
 }
--- test/source/test/FizzBuzzTest.java
+++ test/source/test/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import dssc.FizzBuzz;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,9 +9,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
-    @Test
-    void ThreeIsFizz(){assertThat(FizzBuzz.test("3"), is("fizz")); }
-    @Test
-    void FiveIsBuzz(){assertThat(FizzBuzz.test("5"), is("buzz")); }
+
+    @Test    void EmptyString(){check(0,""); }
+    @Test    void ThreeIsFizz(){check(3,"Fizz"); }
+    @Test    void FiveIsBuzz(){check(5,"Buzz"); }
+    @Test    void Multiple3and5(){check(15,"FizzBuzz");}
+    @Test    void SevenIsBang(){check(7,"Bang"); }
+    @Test    void Multiple3and7(){check(21,"FizzBang");}
+    @Test    void Multiple5and7(){ check(35,"BuzzBang");}
+    @Test    void Multiple3and5and7(){check(105,"FizzBuzzBang");}
+
+    private void check(int i, String s) {
+        FizzBuzz FizzBuzzBang = new FizzBuzz();
+        assertEquals(s, FizzBuzzBang.test(i));
+    }
 
 }
